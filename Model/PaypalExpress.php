b3f4753e80ed0ef8bc83e9783b928dc0a78af5ec
landofcoder <landofcoder@gmail.com>
2021-12-23 11:57:04 +0700

    updated files

---
+++ Model/PaypalExpress.php
@@ -0,0 +1,77 @@
+<?php
+/**
+  * Landofcoder
+  *
+  * NOTICE OF LICENSE
+  *
+  * This source file is subject to the Landofcoder.com license that is
+  * available through the world-wide-web at this URL:
+  * https://landofcoder.com/terms
+  *
+  * DISCLAIMER
+  *
+  * Do not edit or add to this file if you wish to upgrade this extension to newer
+  * version in the future.
+  *
+  * @category   Landofcoder
+  * @package    Lofmp_MarketplacePayment
+  * @copyright  Copyright (c) 2021 Landofcoder (https://www.landofcoder.com/)
+  * @license    https://landofcoder.com/terms
+ */
+
+namespace Lofmp\MarketplacePayment\Model;
+
+/**
+ * Class PaypalExpress
+ *
+ * @package Lofmp\MarketplacePayment\Model
+ */
+class PaypalExpress extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    /**
+     * Payment code
+     *
+     * @var string|bool
+     */
+    const CODE                  = 'marketplace_paypal';
+
+    /**
+     * @var string
+     */
+    protected $_code            = self::CODE;
+
+    /**
+     * @var bool
+     */
+    protected $_isOffline       = true;
+
+    /**
+     * @var bool
+     */
+    protected $_canUseCheckout  = false;
+
+    /**
+     * @var bool
+     */
+    protected $_canUseInternal  = true;
+
+    /**
+     * Get pre select option from config
+     *
+     * @return bool|int|string
+     */
+    public function getDataPreSelect()
+    {
+        return 1;
+    }
+
+    /**
+     * Get Auto Create Invoice option from config
+     *
+     * @return bool
+     */
+    public function getDataAutoCreateInvoice()
+    {
+        return false;
+    }
+}
