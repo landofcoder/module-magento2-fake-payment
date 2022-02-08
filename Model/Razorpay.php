dde966e78d6b8736228e259bef3b8357abd5137f
khanhnhu2512 <luongvannhu2512@gmail.com>
2022-02-08 16:35:41 +0700

    update for razorpay

--- Model/Razorpay.php
+++ Model/Razorpay.php
@@ -1,25 +1,27 @@
 <?php
 /**
-  * Landofcoder
-  *
-  * NOTICE OF LICENSE
-  *
-  * This source file is subject to the Landofcoder.com license that is
-  * available through the world-wide-web at this URL:
-  * https://landofcoder.com/terms
-  *
-  * DISCLAIMER
-  *
-  * Do not edit or add to this file if you wish to upgrade this extension to newer
-  * version in the future.
-  *
-  * @category   Landofcoder
-  * @package    Lofmp_MarketplacePayment
-  * @copyright  Copyright (c) 2021 Landofcoder (https://www.landofcoder.com/)
-  * @license    https://landofcoder.com/terms
+ * Landofcoder
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Landofcoder.com license that is
+ * available through the world-wide-web at this URL:
+ * https://landofcoder.com/terms
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category   Landofcoder
+ * @package    Lofmp_MarketplacePayment
+ * @copyright  Copyright (c) 2021 Landofcoder (https://www.landofcoder.com/)
+ * @license    https://landofcoder.com/terms
  */
 
 namespace Lofmp\MarketplacePayment\Model;
+
+use Lofmp\SplitOrder\Model\Quote\Payment\RazorpayPayment;
 
 /**
  * Class Razorpay
@@ -28,32 +30,26 @@ namespace Lofmp\MarketplacePayment\Model;
  */
 class Razorpay extends \Magento\Payment\Model\Method\AbstractMethod
 {
-    /**
-     * Payment code
-     *
-     * @var string|bool
-     */
-    const CODE                  = 'marketplace_razorpay';
 
     /**
      * @var string
      */
-    protected $_code            = self::CODE;
+    protected $_code = RazorpayPayment::FAKE_PAYMENT_METHOD;
 
     /**
      * @var bool
      */
-    protected $_isOffline       = true;
+    protected $_isOffline = true;
 
     /**
      * @var bool
      */
-    protected $_canUseCheckout  = false;
+    protected $_canUseCheckout = false;
 
     /**
      * @var bool
      */
-    protected $_canUseInternal  = true;
+    protected $_canUseInternal = true;
 
     /**
      * Get pre select option from config
